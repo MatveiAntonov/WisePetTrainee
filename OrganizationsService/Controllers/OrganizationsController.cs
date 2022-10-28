7d98e492a6f41403671d068b8faafec41fcf5929
ProsTuha <tuhalf2002@mail.ru>
2022-10-28 15:03:26 +0300

    Initialized service

---
+++ OrganizationsService/Controllers/OrganizationsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrganizationsService.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class OrganizationsController : ControllerBase {
+        [HttpGet]
+        public void Get() {
+            // Some code will be there
+        }
+
+        [HttpPost]
+        public void Post() {
+            // Some code will be there
+        }
+
+        [HttpPut]
+        public void Put() {
+            // Some code will be there
+        }
+
+        [HttpDelete]
+        public void Delete() {
+            // Some code will be there
+        }
+    }
+}

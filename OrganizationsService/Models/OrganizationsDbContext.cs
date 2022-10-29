ee1595018b0a7071dbd91f6e2bac8a51aa91f06b
ProsTuha <tuhalf2002@mail.ru>
2022-10-29 18:19:28 +0300

    Connected db and added dbContext

---
+++ OrganizationsService/Models/OrganizationsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrganizationsService.Models {
+    public class OrganizationsDbContext : DbContext {
+        public OrganizationsDbContext(DbContextOptions<OrganizationsDbContext> options)
+            : base(options) { }
+
+        public DbSet<Organization> Organizations => Set<Organization>();
+    }
+}
--- OrganizationsService/Program.cs
+++ OrganizationsService/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using OrganizationsService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<OrganizationsDbContext>(opts => {
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:OrganizationsConnection"]);
+});
 
 var app = builder.Build();
 
